3071a3023af11704eccf79bf0cf9f8cc94e25679
Wanderson Andrade <atendimento@sitepratico.com>
2023-01-09 02:10:00 -0300

    Adding first changes to Anna's App

---
+++ Client/wwwroot/js/JavaScript.js
@@ -0,0 +1,20 @@
+﻿
+function timeOver() {
+    var waitTime = 8000;
+    var alertTime = 3000;
+    var className = Math.random();
+    var colorBase = "#fff";
+
+    document.body.style.background = colorBase;
+    document.body.className = className;
+
+    setTimeout(() => {
+        if (document.body.className != className) return;
+        document.body.style.background = "#aaa";
+    }, waitTime);
+
+    setTimeout(() => {
+        if (document.body.className != className) return;
+        document.body.style.background = colorBase;
+    }, waitTime + alertTime);
+}
